747353e376d0064d189f954ab295e37676346035
gjckd <gjckd@LAPTOP-CTOTK8MF>
2017-03-23 11:51:38 +0900

    /..

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -16,34 +16,34 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Car car = new Car();
-		//현대자동차
+		// 현대자동차
 		System.out.println("<<자동차 목록>>");
 		System.out.println("제조사명 : " + car.getCompany());
 		System.out.println("모델명 : " + car.getModel());
 		System.out.println("색상 : " + car.getColor());
 		System.out.println("최대속도 : " + car.getMaxSpeed() + "km");
 		System.out.printf("가격 : " + "%,d" + "원\n\n", car.getPrice());
-		
+
 		Car car2 = new Car();
-		//기아자동차
-		
+		// 기아자동차
+
 		car2.setCompany("기아자동차");
 		car2.setModel("K7");
 		car2.setColor("흰색");
 		car2.setMaxSpeed(246);
 		car2.setPrice(40000000);
-	
+
 		System.out.println("제조사명 : " + car2.getCompany());
 		System.out.println("모델명 : " + car2.getModel());
 		System.out.println("색상 : " + car2.getColor());
 		System.out.println("최대속도 : " + car2.getMaxSpeed() + "km");
 		System.out.printf("가격 : " + "%,d" + "원\n\n", car2.getPrice());
-		
+
 		Car car3 = new Car();
-		//삼성자동차
-		
+		// 삼성자동차
+
 		car3.setCompany("삼성자동차");
 		car3.setModel("SM7");
 		car3.setColor("회색");
