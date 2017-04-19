6995e60973b89415ad5f28359e78529ead15beea
gjckd <gjckd@LAPTOP-CTOTK8MF>
2017-04-19 12:46:02 +0900

    ..

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBankTest.java
@@ -0,0 +1,27 @@
+package org.dimigo.oop;
+
+public class PiggyBankTest {
+
+	public static void main(String[] args) {
+		
+		// 가족 총 인원수 Cnt++, 가족 객체 생성
+		FamilyMember dad = new FamilyMember("아빠");
+		FamilyMember mom = new FamilyMember("엄마");
+		FamilyMember me = new FamilyMember("나");
+		FamilyMember bro = new FamilyMember("남동생");
+		
+		FamilyMember.printMemberCnt();
+		
+		PiggyBank.putMoney(dad, 10000);
+		PiggyBank.putMoney(mom, 5000);
+		PiggyBank.putMoney(me, 2000);
+		PiggyBank.putMoney(bro, 1000);
+		
+		PiggyBank.printBalance();
+
+		PiggyBank.putMoney(me, 1000);
+
+		PiggyBank.printBalance();
+	}
+
+}
