10994c6cd65525028106de6716c9b5b488df9152
gjckd <gjckd@LAPTOP-CTOTK8MF>
2017-05-15 10:48:26 +0900

    .

--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -7,7 +7,7 @@ public class SmartPhoneTest {
 		SmartPhone[] smartphones = { new Galaxy("갤럭시 S8", "삼성", 800000), new IPhone("iPhone 7", "애플", 900000) };
 
 		for (SmartPhone s : smartphones) {
-			System.out.println(s);;
+			System.out.println(s);
 			s.turnOn();
 			s.pay();
 			if (s instanceof Galaxy) {
