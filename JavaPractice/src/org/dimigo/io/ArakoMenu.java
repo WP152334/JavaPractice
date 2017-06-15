20679cb5174dc01c0af199aff40e1d0380c99d0f
gjckd <gjckd@LAPTOP-CTOTK8MF>
2017-06-15 11:34:35 +0900

    ..

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,37 @@
+package org.dimigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
+public class ArakoMenu {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+				BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+
+			// 키보드로 메뉴를 한 줄씩 입력받아 파일에 저장하기
+			// Ctrl+Z 또는 Ctrl+D를 누를 때까지 계속 입력 받기
+			// 한 줄씩 입력 : BufferedReader의 readLine() 이용하기
+
+			String data;
+
+			while ((data = br.readLine()) != null) {
+				writer.write(data + "\n");
+			}
+			System.out.println("<< 메뉴 출력 >>"); // 파일에서 한 줄씩 읽어와서 모니터로 출력하기
+			writer.flush();
+
+			while ((data = reader.readLine()) != null) {
+				System.out.println(data);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
